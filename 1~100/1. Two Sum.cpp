6f4c7758f4ae7a732bce43054c428570a7e609fe
Katrina Hung <70788645+ZaWarudo87@users.noreply.github.com>
2024-12-04 20:56:59 +0800

    Update 1. Two Sum.cpp for O(N logN)

--- 1~100/1. Two Sum.cpp
+++ 1~100/1. Two Sum.cpp
@@ -1,11 +1,28 @@
 class Solution {
+private:
+    static bool comp(pair<int, int> a, pair<int, int> b){
+        return a.first < b.first;
+    }
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0, len = nums.size(); i < len; i++){
-            for(int j = i+1; j < len; j++){
-                if(nums[i] + nums[j] == target)
-                    return {i, j};
-            }
+        int len = nums.size();
+        vector<pair<int, int> > srt(len);
+        vector<int> fir(len), sec(len);
+        for(int i = 0; i < len; i++){
+            srt[i].first = nums[i];
+            srt[i].second = i;
+        }
+        sort(srt.begin(), srt.end(), Solution::comp);
+        for(int i = 0; i < len; i++){
+            fir[i] = srt[i].first;
+            sec[i] = srt[i].second;
+        }
+
+        vector<int>::iterator ans;
+        for(vector<int>::iterator i = fir.begin(); i != fir.end(); i++){
+            ans = lower_bound(i + 1, fir.end(), target - *i);
+            if(ans != fir.end() && *ans == target - *i)
+                return {sec[i - fir.begin()], sec[ans - fir.begin()]};
         }
         return {};
     }
