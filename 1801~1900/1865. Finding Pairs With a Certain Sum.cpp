3939728a2820fdf72d0ff2481a768a9bd3b89ab4
Katrina Hung <70788645+ZaWarudo87@users.noreply.github.com>
2025-07-06 22:47:56 +0800

    Create 1865. Finding Pairs With a Certain Sum.cpp

---
+++ 1801~1900/1865. Finding Pairs With a Certain Sum.cpp
@@ -0,0 +1,34 @@
+class FindSumPairs {
+private:
+    vector<int> n1, n2;
+    unordered_map<int, int> m2;
+
+public:
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        n1 = nums1;
+        n2 = nums2;
+        for(int &i : nums2)
+            ++m2[i];
+    }
+    
+    void add(int index, int val) {
+        --m2[n2[index]];
+        n2[index] += val;
+        ++m2[n2[index]];
+    }
+    
+    int count(int tot) {
+        int ans = 0;
+        for(int &i : n1)
+            if(m2.count(tot - i))
+                ans += m2[tot - i];
+        return ans;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
